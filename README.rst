90350afef5afbcdb2a7f5c49b5e7329aec629ebc
Khurshid Fayzullaev <khurshidfayzullaev@yahoo.com>
2016-05-15 17:09:55 -0400

    Add Python packaging (setuptools)
    - Configure setup.py and add README.rst for PyPI.
    - Modify .gitignore to exclude Python egg metadata, build and distribution folders.

---
+++ README.rst
@@ -0,0 +1,7 @@
+Zype
+--------
+
+To use (with caution), simply do::
+
+    >>> from zype import Zype
+    >>> client = Zype(api_key="<YOUR API KEY>")
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='zype',
+      version='0.1.0',
+      description='Zype API Python Client',
+      classifiers=[
+          'Development Status :: 2 - Pre-Alpha',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Apache Software License',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
+      keywords='zype api client',
+      url='http://github.com/khfayzullaev/zype-python',
+      author='Khurshid Fayzullaev',
+      author_email='khurshidfayzullaev@yahoo.com',
+      license='Apache License 2.0',
+      packages=['zype'],
+      install_requires=[
+          'requests',
+      ],
+      zip_safe=False)
