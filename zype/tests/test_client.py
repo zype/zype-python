9ca6386a5549915fe39a34228436d6a3885faea5
Khurshid Fayzullaev <khurshidfayzullaev@yahoo.com>
2016-06-04 18:23:03 -0400

    Add multiple authentication methods
    There are multiple ways to authenticate to the Zype API (http://dev.zype.com/api_docs/intro/): API keys, app keys, and access tokens.
    Note that only one of the auth types can be utilized.

--- zype/tests/test_client.py
+++ zype/tests/test_client.py
@@ -1,6 +1,8 @@
 import os
 from ConfigParser import SafeConfigParser
 from unittest import TestCase
+from nose.tools import raises
+
 from zype import Zype
 
 
@@ -11,10 +13,22 @@ class TestClient(TestCase):
         config.read(os.path.join(os.path.abspath(
             os.path.dirname(__file__)), '', 'config.ini'))
         self.api_key = config.get("KEYS", "APIKey")
+        self.app_key = config.get("KEYS", "APPKey")
 
-    def test_get_all_videos(self):
+    @raises(ValueError)
+    def test_only_one_auth_type(self):
+        client = Zype(
+            api_key=self.api_key, app_key=self.app_key)
+
+    def test_get_all_videos_api_key(self):
         client = Zype(
             api_key=self.api_key)
+        videos = client.get("videos")
+        self.assertIsNotNone(videos)
+
+    def test_get_all_videos_app_key(self):
+        client = Zype(
+        app_key=self.app_key)
         videos = client.get("videos")
         self.assertIsNotNone(videos)
 
--- zype/zype.py
+++ zype/zype.py
@@ -7,20 +7,38 @@ from resource import create_resource
 
 class Zype(object):
     api_entry_point = "api.zype.com"
-    api_key = ""
+    auth_type = ""
+    auth_val = ""
     protocol = "https://"
 
     def __init__(self, **kwargs):
-        if 'api_key' in kwargs:
-            self._set_up(kwargs)
-        else:
-            raise ValueError("At least an api_key must be provided.")
+        self._set_up(kwargs)
 
     def _set_up(self, settings):
         self.settings = settings
-        self.api_key = self.settings.get('api_key')
+
         self.api_entry_point = self.settings.get(
             'api_entry_point', self.api_entry_point)
+        self.protocol = self.settings.get(
+            'protocol', self.protocol)
+
+        self._set_up_auth(self.settings)
+
+    def _set_up_auth(self, settings):
+
+        self.auth_type, self.auth_val = self._only_one_auth_type(settings)
+        if not self.auth_type or not self.auth_val:
+            raise ValueError(
+                "At least and and most one of the auth options (api_key, app_key, and access_token) must be provided.")
+
+    def _only_one_auth_type(self, settings):
+        default_auth_types = ['app_key', 'api_key', 'access_token']
+        auth_type = [key for key in default_auth_types if key in settings]
+        auth_type_count = len(auth_type)
+        if auth_type_count == 0 or auth_type_count > 1:
+            return (None, None)
+        else:
+            return (auth_type[0], settings[auth_type[0]])
 
     def __getattr__(self, http_method, **kwargs):
         if http_method not in ('get', 'post', 'put', 'delete'):
@@ -29,7 +47,7 @@ class Zype(object):
 
     def _request(self, http_method, resource, **kwargs):
         uri = self._build_resource_path(resource)
-        kwargs.update(dict(api_key=self.api_key))
+        kwargs.update({self.auth_type: self.auth_val})
 
         try:
             result = make_request(http_method, uri, kwargs)
@@ -37,15 +55,14 @@ class Zype(object):
             print "Error (zype): " + e.message
             return None
         else:
-            return _handle_result(result)
+            return self._handle_result(result)
 
     def _build_resource_path(self, resource):
         resource = resource.rstrip('/').lstrip('/')
         return '%s%s/%s' % (self.protocol, self.api_entry_point, resource)
 
-
-def _handle_result(result):
-    if result.content:
-        return create_resource(result.content)
-    else:
-        return None
+    def _handle_result(self, result):
+        if result.content:
+            return create_resource(result.content)
+        else:
+            return None
